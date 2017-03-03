5282a22019edaabacfbef677c2375e52fae08581
gatchi <christen.got@gmail.com>
2017-03-03 00:32:29 -0800

    m: restructure
    cleaner code

--- admin-client/admin-client.c
+++ admin-client/admin-client.c
@@ -32,6 +32,9 @@
 #define CLIENT_KEY_INDEX    152
 #define CLIENT_KEY_LEN       48
 
+void netStartup();
+SOCKET dock();
+void catchCryptPacket(SOCKET socket, unsigned char * receive_buffer, unsigned char * server_key, unsigned char * client_key);
 void dumpx (unsigned char * string, int string_length);
 unsigned char * pollmesg (SOCKET socket_with_something_to_say);
 void sendmesg (SOCKET socket_to_send_via, char * message, int message_length);
@@ -39,69 +42,22 @@ int extractkey (unsigned char * packet03, unsigned char * key, int key_index, in
 
 int main ()
 {
+	SOCKET blocksock;
+	unsigned char * recvbuff = calloc (MAX_PACKET_SIZE, sizeof(char));
+	unsigned char * serverkey = (unsigned char *) calloc (SERVER_KEY_LEN, SERVER_KEY_LEN * sizeof(char));
+	unsigned char * clientkey = (unsigned char *) calloc (CLIENT_KEY_LEN, CLIENT_KEY_LEN * sizeof(char));
 	//---- Connect to ship ------//
 	
 	// Negotiate with winsock (windows networking) to get winsock data by providing version request
-	WSADATA winsock_data;
-	if ( !WSAStartup(MAKEWORD(2,2), &winsock_data) )
-		printf ("WSAStartup a success.\n");
-	else
-		printf ("Could not negotiate with winsock.\n");
-	
-	// Make a socket
-	SOCKET blocksock = socket (AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (blocksock == INVALID_SOCKET)
-		printf ("Whoops, fucked up socket: %ld\n", WSAGetLastError());
-	else
-		printf ("Socket created.\n");
-	
-	// Setup block sockaddr to represent the block server
-	struct sockaddr_in bsa;
-	bsa.sin_family = AF_INET;
-	bsa.sin_addr.s_addr = inet_addr (SHIP_URL);
-	bsa.sin_port = htons (BLOCK1_PORT);
-	
-	// Try to connect to a ship block
-	int result = connect (blocksock, (struct sockaddr *) &bsa, sizeof(bsa));
-	if (result <0)
-		printf ("Can't connect: %d\n", WSAGetLastError());
-	else
-		printf ("Connection made to a block.\n");
+	netStartup();
+	blocksock = dock();
+	printf ("\n");
 	
 	//---- Start encryption --------//
 	
 	// Receive first message (which should be encryption start packet (p03)
-	unsigned char * recvbuff;
-	unsigned char * serverkey;
-	unsigned char * clientkey;
-	
-	serverkey = (unsigned char *) calloc (SERVER_KEY_LEN, SERVER_KEY_LEN * sizeof(char));
-	clientkey = (unsigned char *) calloc (CLIENT_KEY_LEN, CLIENT_KEY_LEN * sizeof(char));
-	
-	recvbuff = pollmesg (blocksock);
-	
-	if (recvbuff == NULL)
-	{
-		printf ("Connection error while waiting for encryption packet. Aborted.\n");
-		return 1;
-	}
-	
-	if (recvbuff[0] == 0xC8)  // 200 (size of packet)
-	{
-		// This is almost certainly it
-		extractkey (recvbuff, serverkey, SERVER_KEY_INDEX, SERVER_KEY_LEN);
-		extractkey (recvbuff, clientkey, CLIENT_KEY_INDEX, CLIENT_KEY_LEN);
-	}
-	else
-	{
-		printf ("Received wrong packet. Are you sure youre connecting to the right server?\n");
-		return 2;
-	}
-	
-	dumpx (serverkey, SERVER_KEY_LEN);
-	dumpx (clientkey, CLIENT_KEY_LEN);
-	
-	// Lets try disconnecting with a disconnect packet
+	printf ("Listening for crypt packet...\n");
+	catchCryptPacket(blocksock, recvbuff, serverkey, clientkey);
 	
 	// Setup encryption
 	PSO_CRYPT * hiscipher = calloc (1, sizeof(PSO_CRYPT));
@@ -184,6 +140,67 @@ int extractkey (unsigned char * in, unsigned char * key, int keyloc, int keylen)
 	return i;
 }
 
+void catchCryptPacket (SOCKET sock, unsigned char * buff, unsigned char * serverkey, unsigned char * clientkey)
+{	
+	buff = pollmesg (sock, buff);
+	
+	if (buff == NULL)
+	{
+		printf ("Connection error while waiting for encryption packet. Aborted.\n");
+		free (buff);
+		exit(CONNECTION_ERROR);
+	}
+	if (buff[0] == 0xC8)  // 200 (size of packet)
+	{
+		// This is almost certainly it
+		printf ("Caught packet.\nExtracting keys...\n");
+		extractkey (buff, serverkey, SERVER_KEY_INDEX, SERVER_KEY_LEN);
+		extractkey (buff, clientkey, CLIENT_KEY_INDEX, CLIENT_KEY_LEN);
+		free (buff);
+		printf ("Finished.\n\n");
+	}
+	else
+	{
+		printf ("Received wrong packet. Are you sure youre connecting to the right server?\n");
+		free (buff);
+		exit(WRONG_PACKET_ERROR);
+	}
+}
+
+SOCKET dock()
+{
+	// Make a socket
+	SOCKET sock = socket (AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (sock == INVALID_SOCKET)
+		printf ("Whoops, fucked up socket: %ld\n", WSAGetLastError());
+	else
+		printf ("Socket created.\n");
+	
+	// Setup block sockaddr to represent the block server
+	struct sockaddr_in bsa;
+	bsa.sin_family = AF_INET;
+	bsa.sin_addr.s_addr = inet_addr (SHIP_URL);
+	bsa.sin_port = htons (BLOCK1_PORT);
+	
+	// Try to connect to a ship block
+	int result = connect (sock, (struct sockaddr *) &bsa, sizeof(bsa));
+	if (result <0)
+		printf ("Can't connect: %d\n", WSAGetLastError());
+	else
+		printf ("Connection made to a block.\n");
+	
+	return sock;
+}
+
+void netStartup()
+{
+	WSADATA winsock_data;
+	if ( !WSAStartup(MAKEWORD(2,2), &winsock_data) )
+		printf ("WSAStartup a success.\n");
+	else
+		printf ("Could not negotiate with winsock.\n");
+}
+
 void dumpx (unsigned char * in, int len)
 {
 	printf ("\n");
@@ -200,7 +217,7 @@ void dumpx (unsigned char * in, int len)
 		}
 	}
 	printf ("\n");
-}	
+}
 
 // Toggle announce
 // void toggleAnnounce (CLIENT * c)
