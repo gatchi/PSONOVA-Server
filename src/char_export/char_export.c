8cc4dbcc36b053f9d10af0bc1d4b10f649ec579e
gatchi <christen.got@gmail.com>
2017-05-16 14:52:49 -0700

    made char_export utility linux usable
    i think, at least.  doesnt matter much since this utility is massively
    inferior to just doing a mysql dump n restore for transferring data.
    not a single utility exists for restoring character data created by this
    utility.  might just delete it from the repo

--- src/char_export/char_export.c
+++ src/char_export/char_export.c
@@ -9,7 +9,14 @@
 /****************************************************************/
 
 
+#ifdef _WIN32
 #include	<windows.h>
+#endif
+
+#ifdef __gnu_linux__
+#include	<stdlib.h>
+#endif
+
 #include	<stdio.h>
 #include	<string.h>
 #include	<time.h>
@@ -51,104 +58,105 @@ main( int argc, char * argv[] )
 		printf ("The configuration file tethealla.ini appears to be missing.\n");
 		return 1;
 	}
-	else
-		while (fgets (&config_data[0], 255, fp) != NULL)
+	
+	while ((fgets (config_data, 255, fp) != NULL) && (config_index < 5))
+	{
+		if (config_data[0] != 0x23)
 		{
-			if (config_data[0] != 0x23)
+			if (config_index < 0x04)
 			{
-				if (config_index < 0x04)
+				// Remove newlines and any carriage returns
+				char len = strlen (&config_data[0]);
+				if (config_data[len-1] == '\n')
 				{
-					ch = strlen (&config_data[0]);
-					if (config_data[ch-1] == 0x0A)
-						config_data[ch--]  = 0x00;
-					config_data[ch] = 0;
+					config_data[len-1]  = '\0';
+					if (config_data[len-2] == '\r');
+						config_data[len-2] = '\0';
 				}
-				switch (config_index)
-				{
-				case 0x00:
-					// MySQL Host
-					memcpy (&mySQL_Host[0], &config_data[0], ch+1);
-					break;
-				case 0x01:
-					// MySQL Username
-					memcpy (&mySQL_Username[0], &config_data[0], ch+1);
-					break;
-				case 0x02:
-					// MySQL Password
-					memcpy (&mySQL_Password[0], &config_data[0], ch+1);
-					break;
-				case 0x03:
-					// MySQL Database
-					memcpy (&mySQL_Database[0], &config_data[0], ch+1);
-					break;
-				case 0x04:
-					// MySQL Port
-					mySQL_Port = atoi (&config_data[0]);
-					break;
-				default:
-					break;
-				}
-				config_index++;
 			}
-		}
-		fclose (fp);
-
-		if (config_index < 5)
-		{
-			printf ("tethealla.ini seems to be corrupted.\n");
-			return 1;
-		}
-
-		if ( (myData = mysql_init((MYSQL*) 0)) && 
-			mysql_real_connect( myData, &mySQL_Host[0], &mySQL_Username[0], &mySQL_Password[0], NULL, mySQL_Port,
-			NULL, 0 ) )
-		{
-			if ( mysql_select_db( myData, &mySQL_Database[0] ) < 0 ) {
-				printf( "Can't select the %s database !\n", mySQL_Database ) ;
-				mysql_close( myData ) ;
-				return 2 ;
+			switch (config_index)
+			{
+			case 0x00:
+				// MySQL Host
+				strcpy (&mySQL_Host[0], &config_data[0]);
+				break;
+			case 0x01:
+				// MySQL Username
+				strcpy (&mySQL_Username[0], &config_data[0]);
+				break;
+			case 0x02:
+				// MySQL Password
+				strcpy (&mySQL_Password[0], &config_data[0]);
+				break;
+			case 0x03:
+				// MySQL Database
+				strcpy (&mySQL_Database[0], &config_data[0]);
+				break;
+			case 0x04:
+				// MySQL Port
+				mySQL_Port = atoi (&config_data[0]);
+				break;
+			default:
+				break;
 			}
+			config_index++;
 		}
-		else {
-			printf( "Can't connect to the mysql server (%s) on port %d !\nmysql_error = %s\n",
-				mySQL_Host, mySQL_Port, mysql_error(myData) ) ;
+		
+		// Clear config_data after each fgets
+		memset (config_data, 0, 255 * sizeof (char));
+	}
+	fclose (fp);
 
+	if ( (myData = mysql_init((MYSQL*) 0)) && 
+		mysql_real_connect( myData, &mySQL_Host[0], &mySQL_Username[0], &mySQL_Password[0], NULL, mySQL_Port,
+		NULL, 0 ) )
+	{
+		if ( mysql_select_db( myData, &mySQL_Database[0] ) < 0 ) {
+			printf( "Can't select the %s database !\n", mySQL_Database ) ;
 			mysql_close( myData ) ;
-			return 1 ;
+			return 2 ;
 		}
+	}
+	else {
+		printf( "Can't connect to the mysql server (%s) on port %d !\nmysql_error = %s\n",
+			mySQL_Host, mySQL_Port, mysql_error(myData) ) ;
 
-		printf ("Tethealla Server Character Export Tool\n");
-		printf ("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
-		printf ("Guild card #: ");
-		scanf ("%s", inputstr );
-		gc_num = atoi ( inputstr );
-		printf ("Slot #: ");
-		scanf ("%s", inputstr );
-		slot = atoi ( inputstr );
-		sprintf (&myQuery[0], "SELECT * from character_data WHERE guildcard='%u' AND slot='%u'", gc_num, slot );
-		// Check to see if that character exists.
-		//printf ("Executing MySQL query: %s\n", myQuery );
-		if ( ! mysql_query ( myData, &myQuery[0] ) )
+		mysql_close( myData ) ;
+		return 1 ;
+	}
+
+	printf ("Tethealla Server Character Export Tool\n");
+	printf ("-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n");
+	printf ("Guild card #: ");
+	scanf ("%s", inputstr );
+	gc_num = atoi ( inputstr );
+	printf ("Slot #: ");
+	scanf ("%s", inputstr );
+	slot = atoi ( inputstr );
+	sprintf (&myQuery[0], "SELECT * from character_data WHERE guildcard='%u' AND slot='%u'", gc_num, slot );
+	// Check to see if that character exists.
+	//printf ("Executing MySQL query: %s\n", myQuery );
+	if ( ! mysql_query ( myData, &myQuery[0] ) )
+	{
+		myResult = mysql_store_result ( myData );
+		num_rows = (int) mysql_num_rows ( myResult );
+		if (num_rows)
 		{
-			myResult = mysql_store_result ( myData );
-			num_rows = (int) mysql_num_rows ( myResult );
-			if (num_rows)
-			{
-				myRow = mysql_fetch_row ( myResult );
-				cf = fopen ("exported.dat", "wb");
-				fwrite (myRow[2], 1, 14752, cf);
-				fclose ( cf );
-				printf ("Character exported!\n");
-			}
-			else
-				printf ("Character not found.\n");
-			mysql_free_result ( myResult );
+			myRow = mysql_fetch_row ( myResult );
+			cf = fopen ("exported.dat", "wb");
+			fwrite (myRow[2], 1, 14752, cf);
+			fclose ( cf );
+			printf ("Character exported!\n");
 		}
 		else
-		{
-			printf ("Couldn't query the MySQL server.\n");
-			return 1;
-		}
-		mysql_close( myData ) ;
-		return 0;
+			printf ("Character not found.\n");
+		mysql_free_result ( myResult );
+	}
+	else
+	{
+		printf ("Couldn't query the MySQL server.\n");
+		return 1;
+	}
+	mysql_close( myData ) ;
+	return 0;
 }
